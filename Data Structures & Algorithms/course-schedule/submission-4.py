a58787e9d4e79ae3b7c530126a7bf1e5c8143e5b
Shazi Bidarian <135661233+shazibid@users.noreply.github.com>
2026-09-17 21:41:04 -0700

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,44 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        #dfs
+
+        #track the prereqs for every course, if ever has itself as a prereq, then return false
+        #if there is ever a cycle, return false
+
+        #for each courses, make a map of all the prerecs
+        courses = {i: [] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            courses[crs].append(pre)
+        
+        visited = set()
+        def dfs(crs):
+            #if we've seen crs
+            if crs in visited:
+                return False
+            #if not prereqs
+            if courses[crs] == []:
+                return True
+            
+            #create seen set
+            visited.add(crs)
+            #for all prerex in course
+            for pre in courses[crs]:
+                #if dfs on pre returns False
+                if not dfs(pre):
+                    return False
+
+            #only tracking the DFS path, so when we come back up, we can pop the course, if another class has this course as a prerec, it will auto True since we know that the path from this course is valid
+            visited.remove(crs)
+            courses[crs] = []
+            
+            return True
+        
+        for crs in courses:
+            if not dfs(crs):
+                return False
+        
+        return True
+                
+
+
+        
